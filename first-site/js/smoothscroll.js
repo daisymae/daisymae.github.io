761928e08f03f4254d9585f8b8d0ec8719a720de
daisymae <daisymae@orcadiafarm.com>
2016-10-26 21:41:30 -0500

    added a fixed-position 'back-to-top' button with smooth scrolling

--- first-site/js/smoothscroll.js
+++ first-site/js/smoothscroll.js
@@ -41,3 +41,25 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
      });
 
 });
+
+// make it scroll for the back-to-top button link also
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+    // offset by 50 so scrolls more to the top of the 'region'
+       scrollTop: $(hash).offset().top -50
+     }, 300, function(){ // number here is the speed; higher # is slower
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
